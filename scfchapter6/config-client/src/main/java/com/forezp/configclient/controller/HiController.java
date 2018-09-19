818cca2b0036f729aa55599992b285d83643fac8
maolixian <maolixian@exfresh.com.cn>
2018-09-19 20:28:57 +0800

    配置服务器

---
+++ scfchapter6/config-client/src/main/java/com/forezp/configclient/controller/HiController.java
@@ -0,0 +1,18 @@
+package com.forezp.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by maolixian on 2018/9/19
+ */
+@RestController
+public class HiController {
+    @Value("${foo}")
+    String foo;
+    @GetMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
